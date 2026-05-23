1e52e79eeb6f822ee699ae248f0a1d2297328740
Rimpy Bhogal <rimpyBhogal09@gmail.com>
2026-05-24 01:03:19 +0530

    navbar_v5

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
       <h1 className="text-5xl font-bold">Hey, I'm Rimpy 👋</h1>
       <p className="mt-4 text-xl text-gray-400">Portfolio coming soon...</p>
+
+      {/* Temporary spacer to test scroll */}
+      <div style={{ height: "200vh" }} />
     </main>
   );
 }
